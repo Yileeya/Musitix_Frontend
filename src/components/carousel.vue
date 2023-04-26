95869a0b5383993324a34ecc8f34291d40344fd4
Yilee <yileefrontend@gmail.com>
2023-04-27 00:39:12 +0800

    [介面]首頁carousel

---
+++ src/components/carousel.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button
+        v-for="indicatorIndex in carouselItems.length"
+        :key="'indicator' + indicatorIndex"
+        type="button"
+        data-bs-target="#carousel"
+        :data-bs-slide-to="indicatorIndex - 1"
+        :class="[{ active: indicatorIndex - 1 === 0 }]"
+        aria-current="true"
+      ></button>
+    </div>
+    <div class="carousel-inner">
+      <div
+        v-for="(item, index) in carouselItems"
+        :key="'img' + item.id"
+        class="carousel-item"
+        :class="[{ active: index === 0 }]"
+        @click.prevent="openHrefLink(item.activityUrl)"
+      >
+        <img :src="item.imageUrl" class="w-100" :alt="item.title" />
+      </div>
+    </div>
+
+    <button
+      v-for="i in 2"
+      :key="'control-btn' + i"
+      :class="[i === 1 ? 'carousel-control-prev' : 'carousel-control-next']"
+      type="button"
+      data-bs-target="#carousel"
+      :data-bs-slide="[i === 1 ? 'prev' : 'next']"
+    >
+      <span
+        :class="[i === 1 ? 'carousel-control-prev-icon' : 'carousel-control-next-icon']"
+        aria-hidden="true"
+      ></span>
+      <span class="visually-hidden">{{ i === 1 ? 'Previous' : 'Next' }}</span>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+export interface CarouselItems {
+  id: number | string
+  title: string
+  imageUrl: string
+  activityUrl: string
+}
+
+defineProps<{
+  carouselItems: CarouselItems[]
+}>()
+
+const openHrefLink = (href: string) => {
+  window.open(href, '_blank')
+}
+</script>
+
+<style scoped lang="scss">
+.carousel-inner {
+  max-height: 600px;
+  display: flex;
+  align-items: center;
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
--- src/views/home/homeLayout.vue
+++ src/views/home/homeLayout.vue
@@ -1,1 +1,11 @@
-<template>首頁</template>
+<template>
+  <carousel :carousel-items="carouselItems" />
+</template>
+<script setup lang="ts">
+import Carousel from '@/components/carousel.vue'
+import type { CarouselItems } from '@/components/carousel.vue'
+import { reactive } from 'vue'
+
+import carouselJson from '@/demoData/home/carouselJson'
+const carouselItems: CarouselItems[] = reactive(carouselJson)
+</script>
