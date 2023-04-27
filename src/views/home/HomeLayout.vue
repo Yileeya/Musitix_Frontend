e3c9157d0bc072653765f25f0c399ee2838df6bb
Yilee <yileefrontend@gmail.com>
2023-04-27 23:26:19 +0800

    修改檔案名稱

--- src/views/home/HomeLayout.vue
+++ src/views/home/HomeLayout.vue
@@ -2,10 +2,11 @@
   <carousel :carousel-items="carouselItems" />
 </template>
 <script setup lang="ts">
-import Carousel from '@/components/carousel.vue'
-import type { CarouselItems } from '@/components/carousel.vue'
+import Carousel from '@/views/home/Carousel.vue'
+import type { CarouselItems } from '@/views/home/Carousel.vue'
 import { reactive } from 'vue'
 
 import carouselJson from '@/demoData/home/carouselJson'
+
 const carouselItems: CarouselItems[] = reactive(carouselJson)
 </script>
