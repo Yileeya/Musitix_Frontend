078de45db2e95239631894f7cd3ed1093b037193
Yilee <yileefrontend@gmail.com>
2023-04-29 21:06:52 +0800

    [介面]首頁_搜尋

--- src/views/home/HomeLayout.vue
+++ src/views/home/HomeLayout.vue
@@ -1,12 +1,19 @@
 <template>
   <carousel :carousel-items="carouselItems" />
+  <search-bar class="container" @change-router-path="changeRouterPath" />
 </template>
 <script setup lang="ts">
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
 import Carousel from '@/views/home/Carousel.vue'
 import type { CarouselItems } from '@/views/home/Carousel.vue'
-import { reactive } from 'vue'
-
 import carouselJson from '@/demoData/home/carouselJson'
+import SearchBar from '@/views/home/SearchBar.vue'
 
 const carouselItems: CarouselItems[] = reactive(carouselJson)
+
+const router = useRouter()
+const changeRouterPath = (path: string) => {
+  router.push(path)
+}
 </script>
---
+++ src/views/home/SearchBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="search-bar">
+    <div class="search-bg">
+      <div class="form-input">
+        <input
+          v-model="search"
+          class="form-control"
+          placeholder="想聽哪個樂團？"
+          @keydown.enter="emitRouterChange"
+        />
+        <i class="fa fa-search" @click.prevent="emitRouterChange" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref, defineEmits } from 'vue'
+
+const search = ref('')
+const emit = defineEmits<{
+  (e: 'changeRouterPath', path: string): void
+}>()
+
+function emitRouterChange() {
+  if (!search.value) return
+  emit('changeRouterPath', '/todo')
+}
+</script>
+
+<style scoped>
+.search-bar {
+  position: relative;
+  margin-top: -1em;
+
+  .search-bg {
+    background-color: #f9f9f9;
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15);
+    border-radius: 48px;
+    padding: 2.5em 0;
+
+    .form-input {
+      width: 80%;
+      margin: auto;
+      position: relative;
+
+      .form-control {
+        border: 2px solid var(--gray);
+        border-radius: 4px;
+        height: 3.5em;
+        padding: 0 15px;
+
+        &:focus {
+          border-color: var(--primary-color);
+        }
+
+        &:focus + .fa-search {
+          color: var(--primary-color);
+        }
+      }
+
+      .fa-search {
+        position: absolute;
+        top: 14px;
+        right: 15px;
+        color: var(--gray);
+        font-size: 30px;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+  }
+}
+</style>
