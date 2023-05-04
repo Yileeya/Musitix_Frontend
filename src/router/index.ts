964a2a4fc27c878b1d94cd09fcb5394917c2f103
Yilee <yileefrontend@gmail.com>
2023-05-04 19:47:15 +0800

    路由改成hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
-import {createRouter, createWebHistory} from "vue-router";
-import routes from "./routes";
-import {beforeEach} from "./routerGuard";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from './routes'
+import { beforeEach } from './routerGuard'
 
 const allRoutes = [
-    ...routes,
-    {
-        path: "/:pathMatch(.*)*",
-        redirect: {name: "home"}, // 如果找不到路由，重導向到首頁
-    }
-];
+  ...routes,
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: 'home' } // 如果找不到路由，重導向到首頁
+  }
+]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: allRoutes
-});
+  history: createWebHashHistory(),
+  routes: allRoutes
+})
 
 // 設定導航守衛
-router.beforeEach(beforeEach);
+router.beforeEach(beforeEach)
 
-export default router;
+export default router
