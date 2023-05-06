a27284f8804614faaba61386d866866a7639b353
Yilee <yileefrontend@gmail.com>
2023-05-07 01:20:57 +0800

    [新增]套件vue-toastification與相關設定

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import router from './router'
 import 'bootstrap'
 import { createPinia } from 'pinia'
 import CKEditor from '@ckeditor/ckeditor5-vue'
+import { toast, options } from './plugins/toastification'
 
 const app = createApp(App)
 
-app.use(createPinia()).use(router).use(CKEditor).mount('#app')
+app.use(createPinia()).use(router).use(CKEditor).use(toast, options).mount('#app')
---
+++ src/plugins/toastification.ts
@@ -0,0 +1,10 @@
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+export const toast = Toast
+export const options = {
+  hideProgressBar: true,
+  position: 'top-center',
+  pauseOnHover: true,
+  draggable: false
+}
