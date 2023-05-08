0889a52fb3ec20978498fd2f04c8a5dfd110e8ad
Yilee <yileefrontend@gmail.com>
2023-05-09 00:11:18 +0800

    [介面]活動頁面 1. 新增HtmlContent組件 2. 調整demo json內容

---
+++ src/components/HtmlContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div v-html="html" class="html-content"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  html: string
+}>()
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding: 3em 15px;
+}
+.html-content :deep(h3) {
+  color: #0b1697;
+  margin-bottom: 15px;
+}
+.html-content :deep(img) {
+  max-width: 480px;
+  width: 100%;
+  margin-bottom: 15px;
+}
+</style>
--- src/views/activity/ActivityLayout.vue
+++ src/views/activity/ActivityLayout.vue
@@ -5,12 +5,14 @@
     :date-range="[activity.startDate, activity.endDate]"
     :sponsor-name="activity.sponsorName"
   />
+  <html-content :html="activity.HtmlContent" />
 </template>
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { ref } from 'vue'
 import activityJson from '@/demoData/activity/activityJson'
 import ActivityInfo from '@/views/activity/ActivityInfo.vue'
+import HtmlContent from '@/components/HtmlContent.vue'
 const route = useRoute()
 //若無此activityId，則回到首頁
 const activity = ref(activityJson)
