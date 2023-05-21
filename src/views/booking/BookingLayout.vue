7aed969ce49f2532e9bdf7c5467c1b6e631e84d9
Yilee <yileefrontend@gmail.com>
2023-05-21 12:41:11 +0800

    [訂票]步驟標題

--- src/views/booking/BookingLayout.vue
+++ src/views/booking/BookingLayout.vue
@@ -1,3 +1,16 @@
 <template>
-購票頁面
+  <div class="booking-layout">
+    <steps-title class="container" />
+  </div>
 </template>
+
+<script setup lang="ts">
+import StepsTitle from '@/views/booking/StepsTitle.vue'
+</script>
+<style scoped lang="scss">
+.booking-layout {
+  background-color: var(--primary-color);
+  padding: 3% 0;
+  min-height: inherit;
+}
+</style>
